9706e91c6e50eb3fcf457a093cc0e9d8a7d3a141
Crowdbotics <engineering@crowdbotics.com>
2023-09-18 09:56:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-field-dev-116978",
+  url: "https://withered-field-dev-116978.botics.co",
+  api: "https://withered-field-dev-116978.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
